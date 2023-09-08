44742d2db766add914af2d44b14b1c8dea64d549
Min0729 <keyti0729@naver.com>
2023-09-08 17:08:51 +0900

    login

---
+++ login_09_5/src/main/java/login_09_5/LogoutServlet.java
@@ -0,0 +1,27 @@
+package login_09_5;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/logOut")
+public class LogoutServlet extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		HttpSession session = req.getSession();
+		
+		session.invalidate();
+		// session.setMaxInactiveInterval(3);	// 3초뒤 로그아웃
+		
+		resp.sendRedirect("/login_09_5/home");
+		
+	}
+	
+}
